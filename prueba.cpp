6f79bb9f87a8323e15137568ec3676a1ffa1a3f8
Alex Calero Revilla <alexcalerorevilla@gmail.com>
2023-05-29 01:27:25 -0500

    El programa prueba.cpp realiza una suma de dos numeros

--- prueba.cpp
+++ prueba.cpp
@@ -2,8 +2,17 @@
 
 using namespace std;
 
+int suma(int a, int b) {
+    return a + b;
+}
+
 int main(void) {
     int a, b;
-    
+
+    cout << "Ingrese dos numeros a sumar: ";
+    cin >> a >> b;
+
+    cout << "La suma de los dos numeros es " << suma(a, b) << endl;
+
     return 0;
 }
